571e69da564e968bac96c77f6b558aa69f74f90b
Arun <ak3930566@gmail.com>
2024-10-31 22:18:22 +0530

    Created user avatar dropdown

--- client/src/components/user-avatar.tsx
+++ client/src/components/user-avatar.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import useAuthStore from "../stores/useAuthStore";
 
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
@@ -10,18 +9,23 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { ChevronDown, ChevronUp } from "lucide-react";
+import {
+  ChevronDown,
+  ChevronUp,
+  LibraryBig,
+  LogOut,
+  Settings,
+} from "lucide-react";
+import { Link } from "react-router-dom";
 
 const UserAvatar = () => {
   const authStore: any = useAuthStore();
   const user = authStore.user;
 
-
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <div
-          className="flex items-center gap-x-3 z-30">
+        <div className="flex items-center gap-x-3 z-30 cursor-pointer">
           <Avatar className="cursor-pointer">
             <AvatarImage src={user?.avatar} />
             <AvatarFallback>{user?.name.charAt(0)}</AvatarFallback>
@@ -34,12 +38,42 @@ const UserAvatar = () => {
         </div>
       </DropdownMenuTrigger>
 
-      <DropdownMenuContent>
-        <DropdownMenuLabel>{user?.name}</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Settings</DropdownMenuItem>
-        <DropdownMenuItem>Logout</DropdownMenuItem>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuItem className="flex items-center font-normal gap-x-2">
+          <Avatar>
+            <AvatarImage src={user?.avatar} />
+            <AvatarFallback>{user?.name.charAt(0)}</AvatarFallback>
+          </Avatar>
+
+          <div className="flex flex-col ml-2">
+            <p className="text-neutral-500">{user?.name}</p>
+            <Link
+              to={"/user/profile"}
+              className="text-blue-500 hover:underline cursor-pointer"
+            >
+              Edit your profile
+            </Link>
+          </div>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem className="cursor-pointer">
+          <Link to={"/user/settings"} className="flex items-center">
+            <Settings className="mr-2" />
+            Account settings
+          </Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem className="cursor-pointer">
+          <Link to={"/user/workspace"} className="flex items-center">
+            <LibraryBig className="mr-2" />
+            Workspaces
+          </Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem className="cursor-pointer flex items-center">
+          <LogOut className="mr-2" />
+          Sign Out
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
