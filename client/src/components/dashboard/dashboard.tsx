69fb0472800a3a8af51f82cd1246c35938073d79
Arun <ak3930566@gmail.com>
2024-11-01 22:48:37 +0530

    added greet function

--- client/src/components/dashboard/dashboard.tsx
+++ client/src/components/dashboard/dashboard.tsx
@@ -1,9 +1,15 @@
+import greetUser from "../../lib/greet-user";
+import useAuthStore from "../../stores/useAuthStore";
+
 const Dashboard = () => {
+  const authStore: any = useAuthStore();
+  const user = authStore.user;
+
   return (
-    <div>
-      User Dashbaord
+    <div className="w-full h-full p-4">
+      <h1 className="font-bold text-xl">{greetUser(user?.name)}</h1>
     </div>
-  )
+  );
 };
 
 export default Dashboard;
